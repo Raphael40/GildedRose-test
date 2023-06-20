f7809c575c62709288610a9f25fffa4b81fca347
Julian Festing <julfesting@gmail.com>
2023-06-20 15:07:31 +0100

    added test for backstage pass, did small refactor but more to be done

--- gilded_rose.test.js
+++ gilded_rose.test.js
@@ -4,6 +4,18 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
+
     expect(items[0].name).toBe("foo");
   });
+
+  it("backstage pass", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 6)]);
+    const items = gildedRose.updateQuality();
+    
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(4);
+    expect(items[0].quality).toBe(9);
+  });
+
+
 });
