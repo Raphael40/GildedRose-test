2760bb83d3272ab6e012f8d07ae9a6b896cc9e19
Julian Festing <julfesting@gmail.com>
2023-06-21 12:02:07 +0100

    full refactor, extracted items into Items class and call each from updateQuality function seperately. All tests pass

--- gilded_rose.js
+++ gilded_rose.js
@@ -6,56 +6,66 @@ class Item {
   }
 }
 
+class Items {
+  regularItem(item) {
+    if (item.quality > 0) {
+      if (item.sellIn < 0) {
+        item.quality = item.quality - 2;
+      } else {
+        item.quality = item.quality - 1;
+      }
+    }
+    item.sellIn = item.sellIn - 1;
+  }
+
+  agedBrie(item) {
+    if (item.quality < 50) {
+      if (item.sellIn <= 0) {
+        item.quality = item.quality + 2;
+      } else {
+        item.quality = item.quality + 1;
+      }
+    }
+    item.sellIn = item.sellIn - 1;
+  }
+
+  concert(item) {
+    if (item.sellIn <= 0) {
+      item.quality = 0
+    } else if (item.sellIn <= 5) {
+      item.quality = item.quality + 3;
+    } else if (item.sellIn <= 10) {
+      item.quality = item.quality + 2;
+    }
+    item.sellIn = item.sellIn - 1;
+  }
+
+}
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
 
   updateQuality() {
-    let concertPass = 'Backstage passes to a TAFKAL80ETC concert'
+    this.items.map(item => {
+      const itemsClass = new Items()
 
-    for (let i = 0; i < this.items.length; i++) {
-      let item = this.items[i]
-      if (item.name != 'Aged Brie' && item.name != concertPass) {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1;
-          }
-        }
+      if (item.name == 'Aged Brie') {
+        itemsClass.agedBrie(item)
+      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        itemsClass.concert(item)
+      } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
+        return
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == concertPass) {
-            if (item.sellIn <= 10) {
-              item.quality = item.quality + 1;
-            }
-            if (item.sellIn <= 5) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
+        itemsClass.regularItem(item)
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie' && item.name != concertPass) {
-            if (item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1;
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    }
+    })
 
     return this.items;
   }
 }
+
 
 module.exports = {
   Item,
